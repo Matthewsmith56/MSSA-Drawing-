4ec5c3710f0acc5eb8bdcf0fe68ee1858200ca83
Matthew <Matthew@DESKTOP-FBBVVQH>
2018-09-29 20:10:58 -0400

    Add project files.

---
+++ Drawing/DrawingPad.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Drawing
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DrawingPad : Page
+    {
+        public DrawingPad()
+        {
+            this.InitializeComponent();
+        }
+
+        private void drawingCanvas_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Point mouseLocation = e.GetPosition(this.drawingCanvas);
+            Squares mySquare = new Squares(100);
+            if (mySquare is IDraw)
+            {
+                IDraw drawSquare = mySquare;
+                drawSquare.SetLocation((int) mouseLocation.X, (int) mouseLocation.Y);
+                drawSquare.Draw(drawingCanvas);
+
+                if (mySquare is IColors)
+                {
+                    IColors colorSquare = mySquare;
+                    colorSquare.SetColor(Colors.BlueViolet);
+                }
+            }
+
+        }
+    }
+}
